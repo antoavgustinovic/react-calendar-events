2838aa1879c2dc2709aa446268064d4a69dbc4b1
Anto Avgustinovic <axa0520@dexcom.com>
2023-06-27 20:05:14 +0200

    project initialization

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import './index.css';
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<App />);
